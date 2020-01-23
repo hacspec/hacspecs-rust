5f79cbb22642662e16303f733826004c528fff28
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-01-23 19:49:40 +0100

    sha256; needs cleanup

--- src/sha2.rs
+++ src/sha2.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 // Import hacspec and all needed definitions.
 use hacspec::prelude::*;
 
@@ -10,30 +9,20 @@ enum Variant {
     SHA512 = 512,
 }
 
-const block_size: usize = 64;
-const k_size: usize = 64;
-const len_size: usize = 8;
-const hash_size: usize = Variant::SHA256 as usize / 8;
+const BLOCK_SIZE: usize = 64;
+const K_SIZE: usize = 64;
+const LEN_SIZE: usize = 8;
+const HASH_SIZE: usize = Variant::SHA256 as usize / 8;
 
-bytes!(Block, block_size);
+bytes!(Block, BLOCK_SIZE);
 bytes!(OpTableType, 12);
-bytes!(digest_t, Variant::SHA256 as usize);
+bytes!(digest_t, HASH_SIZE);
 bytes!(h0_t, 8);
-seq!(RoundConstantsTable, u32, k_size);
+seq!(RoundConstantsTable, u32, K_SIZE);
 seq!(hash_t, WordT, 8);
 
 type LenT = u64;
 type WordT = u32;
-
-// to_len : FunctionType = uint64
-// len_to_bytes : FunctionType = bytes.from_uint64_be
-// to_word : FunctionType = uint32
-// bytes_to_words : FunctionType = bytes.to_uint32s_be
-// words_to_bytes : FunctionType = bytes.from_uint32s_be
-
-// const h0: hash_t = hash_t::from([
-//     0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
-//     0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4]);
 
 fn ch(x: WordT, y: WordT, z: WordT) -> WordT {
     (x & y) ^ ((!x) & z)
@@ -44,21 +33,22 @@ fn maj(x: WordT, y: WordT, z: WordT) -> WordT {
 }
 
 fn sigma(x: WordT, i: usize, op: usize) -> WordT {
-    let opTable = OpTableType::from([2, 13, 22, 6, 11, 25, 7, 18, 3, 17, 19, 10]);
+    let op_table = OpTableType::from([2, 13, 22, 6, 11, 25, 7, 18, 3, 17, 19, 10]);
     let tmp = if op == 0 {
-        x >> opTable[3*i+2]
+        x >> op_table[3*i+2]
     } else {
-        x.rotate_right(u32::from(opTable[3*i+2]))
+        x.rotate_right(u32::from(op_table[3*i+2]))
     };
-    x.rotate_right(u32::from(opTable[3*i])) ^
-    x.rotate_right(u32::from(opTable[3*i+1])) ^
+    x.rotate_right(u32::from(op_table[3*i])) ^
+    x.rotate_right(u32::from(op_table[3*i+1])) ^
     tmp
 }
 
+#[wrappit]
 fn schedule(block: Block) -> RoundConstantsTable {
     let b = block.to_uint32s_be();
     let mut s = RoundConstantsTable::new();
-    for i in 0..k_size {
+    for i in 0..K_SIZE {
         if i < 16 {
             s[i] = b[i];
         }
@@ -75,6 +65,7 @@ fn schedule(block: Block) -> RoundConstantsTable {
     s
 }
 
+#[wrappit]
 fn shuffle(ws:  RoundConstantsTable, hashi: hash_t) -> hash_t {
     let k_table = RoundConstantsTable::from([
         0x428a_2f98, 0x7137_4491, 0xb5c0_fbcf, 0xe9b5_dba5, 0x3956_c25b, 0x59f1_11f1, 0x923f_82a4,
@@ -90,15 +81,15 @@ fn shuffle(ws:  RoundConstantsTable, hashi: hash_t) -> hash_t {
     ]);
 
     let mut h = hashi;
-    for i in 0..k_size {
+    for i in 0..K_SIZE {
         let a0 = h[0];
         let b0 = h[1];
         let c0 = h[2];
-        let d0 = h[3];
+        let d0: WordT = h[3]; // TODO: not sure why we have to add types here
         let e0 = h[4];
         let f0 = h[5];
         let g0 = h[6];
-        let h0 = h[7];
+        let h0: WordT = h[7];
     
         let t1 = h0 + sigma(e0,1,1) + ch(e0,f0,g0) + k_table[i] + ws[i];
         let t2 = sigma(a0,0,1) + maj(a0,b0,c0);
@@ -115,6 +106,7 @@ fn shuffle(ws:  RoundConstantsTable, hashi: hash_t) -> hash_t {
     h
 }
 
+#[wrappit]
 fn compress(block: Block, h_in: hash_t) -> hash_t {
     let s = schedule(block);
     let mut h = shuffle(s, h_in);
@@ -124,10 +116,30 @@ fn compress(block: Block, h_in: hash_t) -> hash_t {
     h
 }
 
-// fn truncate(b: bytes_t(v)) -> digest_t {
-//     let result = array.create(hash_size, 0);
-//     for i in 0..hash_size {
-//         result[i] = b[i];
-//     }
-//     digest_t(result)
-// }
+pub fn hash(msg: ByteSlice) -> digest_t {
+    let mut h = hash_t::from([0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+                              0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]);
+    for block in msg.chunks(BLOCK_SIZE) {
+        if block.len() < BLOCK_SIZE {
+            // Add padding for last block
+            let mut last_block = Block::new();
+            last_block.update_raw(0, block);
+            last_block[block.len()] = 0x80;
+            let len_bist = (msg.len() * 8) as u64;
+            if block.len() < BLOCK_SIZE - LEN_SIZE {
+                last_block.update_raw(BLOCK_SIZE - LEN_SIZE, &u64::to_be_bytes(len_bist));
+                h = compress(last_block, h);
+            } else {
+                let mut pad_block = Block::new();
+                pad_block.update_raw(BLOCK_SIZE - LEN_SIZE, &u64::to_be_bytes(len_bist));
+                h = compress(last_block, h);
+                h = compress(pad_block, h);
+            }
+        } else {
+            // TODO: drop into
+            h = compress(block.into(), h);
+        }
+    }
+
+    digest_t::from(&h.to_bytes_be()[..])
+}
