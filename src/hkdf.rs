a566ace7866d7f9ac2e3fddf675836324fc64434
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-01-24 11:24:58 +0100

    hkdf

--- src/hkdf.rs
+++ src/hkdf.rs
@@ -2,7 +2,8 @@
 use hacspec::prelude::*;
 
 use crate::sha2;
-use crate::hmac::hmac;
+use crate::hmac;
+use hmac::hmac;
 
 const HASH_LEN: usize = sha2::HASH_SIZE;
 bytes!(PRK, HASH_LEN);
@@ -11,7 +12,6 @@ bytes!(PRK, HASH_LEN);
 /// Extract a pseudo-random key from input key material (IKM) and optionally a salt.
 /// Note that salt can be empty Bytes.
 pub fn extract(salt: Bytes, ikm: Bytes) -> PRK {
-    // PRK = HMAC-Hash(salt, IKM)
     let salt = if salt.len() > 0 {
         salt
     } else {
@@ -21,22 +21,32 @@ pub fn extract(salt: Bytes, ikm: Bytes) -> PRK {
     hmac(salt, ikm).raw().into()
 }
 
-/// Expand a key k, using potentially empty info, and output length l.
-/// Key k must be at least of length HASH_LEN.
-/// Output length l can be at most 255*HASH_LEN. 
-pub fn expand(k: ByteSlice, info: ByteSlice, l: usize) -> Bytes {
-    Bytes::new_len(HASH_LEN*2)
+fn build_hmac_txt(t: Bytes, info: Bytes, iteration: u8) -> Bytes {
+    let mut out = Bytes::new();
+    out.extend(t);
+    out.extend(info);
+    out.push(iteration);
+    out
 }
 
-#[test]
-fn test_kat1() {
-    let ikm = "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b";
-    let salt = "000102030405060708090a0b0c";
-    let info = "f0f1f2f3f4f5f6f7f8f9";
-    let l = 42;
-    let expected_prk = "077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5";
-    let expected_okm = "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865";
+/// Expand a key prk, using potentially empty info, and output length l.
+/// Key prk must be at least of length HASH_LEN.
+/// Output length l can be at most 255*HASH_LEN. 
+pub fn expand(prk: Bytes, info: Bytes, l: usize) -> Bytes {
+    let n = div_ceil(l, HASH_LEN);
+    debug_assert!(n < u8::max_value().into());
+    let n = n as u8;
 
-    let prk = extract(Bytes::from(salt), Bytes::from(ikm));
-    assert_eq!(expected_prk, prk.to_hex());
+    let mut t_i = hmac::PRK::new();
+    let mut t = Bytes::new();
+    for i in 0..n {
+        let hmac_txt_in = if i == 0 {
+            build_hmac_txt(Bytes::new(), info.clone(), i+1)
+        } else {
+            build_hmac_txt(Bytes::from(t_i.raw()), info.clone(), i+1)
+        };
+        t_i = hmac(prk.clone(), hmac_txt_in);
+        t.extend_from_slice(t_i.raw());
+    }
+    Bytes::from(&t[0..l])
 }
